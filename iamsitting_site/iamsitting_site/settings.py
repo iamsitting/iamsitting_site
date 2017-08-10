e8af9ffecde5718ee12f4d6903d6f6b3b27d6c18
Carlos <cdsalamanca27@gmail.com>
2017-08-10 19:36:28 +0000

    'Disabled' about me

--- iamsitting_site/iamsitting_site/settings.py
+++ iamsitting_site/iamsitting_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secrets.prod_secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [secrets.ec2_public_ip, 'iamsitting.com']
 
