b7af429de328cb635760043a9c092c6b42027baf
iamsitting <cdsalamanca27@gmail.com>
2019-01-06 17:27:59 -0600

    functional tests and unit test login

--- iamsitting_site/accounts/tests.py
+++ iamsitting_site/accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+  def test_login_returns_correct_html(self):
+    response = self.client.get(reverse('accounts:login'))
+    self.assertTemplateUsed(response, 'registration/login.html')
---
+++ iamsitting_site/functional_tests/tests.py
@@ -0,0 +1,41 @@
+import unittest
+
+from selenium import webdriver
+
+
+class NewVisitorTest(unittest.TestCase):
+
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_open_page(self):
+
+    # user visits website
+    self.browser.get('http://localhost:8000')
+
+    # user notices iamsitting in the title
+    self.assertIn('iamsitting', self.browser.title)
+
+    # user notices website alert
+    alert_text = self.browser.find_element_by_class_name('alert-dismissible').text
+    self.assertIn('To reduce costs', alert_text)
+
+    # dismiss website alert
+    dismiss_button = self.browser.find_element_by_xpath("//button[@data-dismiss='alert']")
+    dismiss_button.click()
+
+  def test_click_to_login(self):
+
+    # user visits website
+    self.browser.get('http://localhost:8000')
+
+    # user clicks on login
+    login_button = self.browser.find_element_by_xpath("/html/body/div[1]/header/a")
+    login_button.click()
+
+
+if __name__ == '__main__':
+  unittest.main(warnings='ignore')
