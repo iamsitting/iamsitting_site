79bb7a9dc0e89ebca2f4326c772a4c5cc5745273
iamsitting <cdsalamanca27@gmail.com>
2019-01-07 17:19:47 -0600

    get contact form working

--- iamsitting_site/iamsitting_site/settings/development.py
+++ iamsitting_site/iamsitting_site/settings/development.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
