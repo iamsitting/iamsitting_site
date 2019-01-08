98c19b868c37aa9634637350e6ace381091777b9
iamsitting <cdsalamanca27@gmail.com>
2019-01-07 21:49:06 -0600

    show email

--- iamsitting_site/home_app/views.py
+++ iamsitting_site/home_app/views.py
@@ -56,7 +56,9 @@ def contact_me(request):
   if request.method == 'POST':
     name = request.POST.get('name')
     email = request.POST.get('email')
-    message = 'This is message from iamsitting.com.\n ----\n' + request.POST.get('message')
+    message = ('This is message sent through iamsitting.com.\n----\n' +
+               'From ' + name + ' (' + email + '):\n' +
+               request.POST.get('message'))
     subject = 'From: ' + name + ' at ' + str(datetime.now())
     send_mail(subject, message, email, ['carlos@iamsitting.com'], fail_silently=False)
     return HttpResponseRedirect("/")
