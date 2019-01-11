76ece76451d784031c6a3cef10292d50c128723a
iamsitting <cdsalamanca27@gmail.com>
2019-01-11 16:34:10 -0600

    increase sleep time

--- iamsitting_site/iamsitting_site/settings/development.py
+++ iamsitting_site/iamsitting_site/settings/development.py
@@ -18,5 +18,3 @@ STATIC_ROOT = ''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-LOGGING = ''
--- scripts/test_suite.sh
+++ scripts/test_suite.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
-cd iamsitting_site
-
-export DJANGO_SETTINGS_MODULE=iamsitting_site.settings.development
+export DJANGO_SETTINGS_MODULE=iamsitting_site.settings.travis
 
 echo "Loading webpack ... ..."
 npm run dev > run_webpack.log 2>&1 &
 WEBPACK_PID=$!
 sleep 2
 while ! grep -qw "Compiled successfully." run_webpack.log; do sleep 5; done
+echo 'Webpack server successfully started.'
 
 echo "Loading Django ... ..."
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver &
+python iamsitting_site/manage.py runserver &
 DJANGO_PID=$!
-sleep 2
+sleep 10
+echo 'Hopefully Django server successfully started.'
 
 echo "testing Django ... ..."
-python manage.py test --verbosity=2
-sleep 2
+python iamsitting_site/manage.py test --verbosity=2
+
 
 echo "killing processes"
 kill -INT $WEBPACK_PID
