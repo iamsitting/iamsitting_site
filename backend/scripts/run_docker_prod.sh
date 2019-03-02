04e82a8bb24c468fc67dfe627d76303d4e191568
iamsitting <cdsalamanca27@gmail.com>
2019-03-02 09:14:14 -0600

    port for heroku

--- backend/scripts/run_docker_prod.sh
+++ backend/scripts/run_docker_prod.sh
@@ -2,4 +2,4 @@
 scripts/wait-for-it.sh localhost:5432 -t 20
 python iamsitting_site/manage.py makemigrations
 python iamsitting_site/manage.py migrate
-python iamsitting_site/manage.py runserver
+python iamsitting_site/manage.py runserver localhost:$PORT
