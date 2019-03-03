473d61809a89a101762c55b1a9c7204e66f1a12b
iamsitting <cdsalamanca27@gmail.com>
2019-03-02 22:01:47 -0600

    begin adding api

--- frontend/libraries/blog/js/app.js
+++ frontend/libraries/blog/js/app.js
@@ -5,5 +5,19 @@ import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  methods: {
+    getPosts: function() {
+      this.loading = true;
+      this.$http.get("api/posts/")
+        .then((response) => {
+          this.posts = response.data;
+          this.loading = false;
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err);
+        })
+      },
+  }
 })
