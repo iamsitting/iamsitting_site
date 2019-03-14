87df7907a1256d506b2bd67a1ad76f63e7e32544
iamsitting <cdsalamanca27@gmail.com>
2019-03-14 13:31:41 -0500

    add cookie to contact me form

--- frontend/libraries/blog/js/components/footer/ContactMeForm.vue
+++ frontend/libraries/blog/js/components/footer/ContactMeForm.vue
@@ -1,23 +1,54 @@
 <template>
-  <form method="post" action="/contact-me/">
+  <form id="contactMeForm" @submit.prevent="processForm">
     <div class="form-group">
       <label for="name">Name</label>
-      <input class="form-control" type="text" name="name" id="name"/>
+      <input v-model="name" class="form-control" type="text" name="name" id="name"/>
     </div>
     <div class="form-group">
       <label for="email">Email</label>
-      <input class="form-control" type="text" name="email" id="email"/>
+      <input v-model="email" class="form-control" type="text" name="email" id="email"/>
     </div>
     <div class="form-group">
       <label for="message">Message</label>
-      <textarea class="form-control" name="message" id="message" rows="3"></textarea>
+      <textarea v-model="message" class="form-control" name="message" id="message" rows="3"></textarea>
     </div>
     <input class="btn btn-primary" type="submit" value="Send Message" onclick="return confirm('Are you sure?');"/></li>
   </form>
 </template>
 
 <script>
+  import Cookies from 'js-cookie'
+  import axios from 'axios'
+  import qs from 'qs'
   export default {
-    name: 'ContactMeForm'    
+    name: 'ContactMeForm',
+    data() {
+      return {
+        name: "",
+        email: "",
+        message: ""
+      }
+    },
+    methods: {
+      processForm: function() {
+        axios({
+          method: 'post',
+          url: '/contact-me/',
+          data: qs.stringify({
+            name: this.name,
+            email: this.email,
+            message: this.message
+          }),
+          headers: {
+            'X-CSRFToken': Cookies.get('csrftoken'),
+            'Content-Type': 'application/x-www-form-urlencoded',
+          }
+        }).then(response => {
+          if (response.status === '200'){
+            $('#contactMeForm').trigger('reset');
+          }
+        });
+      }
+    }
   }
 </script>
