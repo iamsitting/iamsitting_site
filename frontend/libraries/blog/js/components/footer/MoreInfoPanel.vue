981e98071b517011d59aa3a5f6c23828039a0baa
iamsitting <cdsalamanca27@gmail.com>
2019-03-14 22:23:05 -0500

    forgot panel vue

---
+++ frontend/libraries/blog/js/components/footer/MoreInfoPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="more-info">
+    <p>{{content}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MoreInfoPanel',
+    props: [
+      'content'
+    ]
+  }
+</script>
+
+<style lang="scss" scoped>
+  .more-info {
+    //padding: 10% 0 5% 25%;
+    margin-top: 12%;
+    padding-top: 5%;
+    text-align: center;
+    border: 1px solid white;
+  }
+</style>
